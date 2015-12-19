bd1ac5d5c13debf889deb7e63e3bd588c5034f05
Conchylicultor <etiennefg.pot@gmail.com>
2015-12-19 17:48:19 +0100

    Final multiclass prediction, +minor change (figures, comments,...)

---
+++ finalResults_manage.m
@@ -0,0 +1,21 @@
+
+Ytest = Te.predictions;
+
+tblTr = tabulate(Tr.y);
+tblTe = tabulate(Ytest);
+
+length(Tr.predictions)
+length(Te.predictions)
+
+size(Ytest)
+
+bar([tblTr(:,3) tblTe(:,3)]);
+title('Prediction vs Training sample');
+xlabel('Class');
+ylabel('Proportion');
+h = legend('Training', 'Predicted');
+h.Location = 'northwest';
+
+colormap('summer');
+save('pred_multiclass', 'Ytest');
+
