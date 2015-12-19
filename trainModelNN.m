6eeb69a841188a5e0bcbcbdae72b8417e5ead0f9
Etienne Pot <pot@icin3pc39.epfl.ch>
2015-12-19 22:11:43 +0100

    Tune the NN, add figure and visualisation

--- trainModelNN.m
+++ trainModelNN.m
@@ -5,7 +5,7 @@ function [ classVoteTr, classVoteTe ] = trainModelNN( Tr, Te, labels, param )
     % setup NN. The first layer needs to have number of features neurons,
     %  and the last layer the number of classes (here four).
     nn = nnsetup([size(Tr.normX,2) 100 length(labels)]);
-    opts.numepochs =  20;   %  Number of full sweeps through data
+    opts.numepochs =  40;   %  Number of full sweeps through data
     opts.batchsize = 100;  %  Take a mean gradient step over this many samples
 
     % if == 1 => plots trainin error as the NN is trained
@@ -13,18 +13,44 @@ function [ classVoteTr, classVoteTe ] = trainModelNN( Tr, Te, labels, param )
     
     nn.activation_function              = 'sigm';   %  Activation functions of hidden layers: 'sigm' (sigmoid) or 'tanh_opt' (optimal tanh).
     nn.learningRate                     = 2;            %  learning rate Note: typically needs to be lower when using 'sigm' activation function and non-normalized inputs.
-    nn.momentum                         = param;          %  Momentum
-    nn.scaling_learningRate             = 0.9;            %  Scaling factor for the learning rate (each epoch)
-    nn.weightPenaltyL2                  = 0.02;            %  L2 regularization
-    nn.nonSparsityPenalty               = 0.0;            %  Non sparsity penalty
-    nn.sparsityTarget                   = 2.00;         %  Sparsity target
+    nn.momentum                         = 0.3;          %  Momentum
+    nn.scaling_learningRate             = 0.95;            %  Scaling factor for the learning rate (each epoch)
+    nn.weightPenaltyL2                  = 0.03; %0.02 before           %  L2 regularization
+    nn.nonSparsityPenalty               = 0.001;            %  Non sparsity penalty
+    nn.sparsityTarget                   = 0.61;         %  Sparsity target
     nn.inputZeroMaskedFraction          = 0;            %  Used for Denoising AutoEncoders
     nn.dropoutFraction                  = 0;            %  Dropout level (http://www.cs.toronto.edu/~hinton/absps/dropout.pdf)
     nn.testing                          = 0;            %  Internal variable. nntest sets this to one.
     nn.output                           = 'sigm';       %  output unit 'sigm' (=logistic), 'softmax' and 'linear'
 
+    
+% Good parameters: 40 epoc
+% nn.activation_function              = 'sigm';   %  Activation functions of hidden layers: 'sigm' (sigmoid) or 'tanh_opt' (optimal tanh).
+%     nn.learningRate                     = 2;            %  learning rate Note: typically needs to be lower when using 'sigm' activation function and non-normalized inputs.
+%     nn.momentum                         = 0.3;          %  Momentum
+%     nn.scaling_learningRate             = 0.95;            %  Scaling factor for the learning rate (each epoch)
+%     nn.weightPenaltyL2                  = 0.03; %0.02 before           %  L2 regularization
+%     nn.nonSparsityPenalty               = 0.001;            %  Non sparsity penalty
+%     nn.sparsityTarget                   = 0.61;         %  Sparsity target
+%     nn.inputZeroMaskedFraction          = 0;            %  Used for Denoising AutoEncoders
+%     nn.dropoutFraction                  = 0;            %  Dropout level (http://www.cs.toronto.edu/~hinton/absps/dropout.pdf)
+%     nn.testing                          = 0;            %  Internal variable. nntest sets this to one.
+%     nn.output                           = 'sigm';
+%    nn.learningRate = 2;
 
-    nn.learningRate = 2;
+% Good parameters: 40
+% nn.activation_function              = 'sigm';   %  Activation functions of hidden layers: 'sigm' (sigmoid) or 'tanh_opt' (optimal tanh).
+%     nn.learningRate                     = 2;            %  learning rate Note: typically needs to be lower when using 'sigm' activation function and non-normalized inputs.
+%     nn.momentum                         = 0.3;          %  Momentum
+%     nn.scaling_learningRate             = 0.9;            %  Scaling factor for the learning rate (each epoch)
+%     nn.weightPenaltyL2                  = 0.03; %0.02 before           %  L2 regularization
+%     nn.nonSparsityPenalty               = 0.0;            %  Non sparsity penalty
+%     nn.sparsityTarget                   = 0.61;         %  Sparsity target
+%     nn.inputZeroMaskedFraction          = 0;            %  Used for Denoising AutoEncoders
+%     nn.dropoutFraction                  = 0;            %  Dropout level (http://www.cs.toronto.edu/~hinton/absps/dropout.pdf)
+%     nn.testing                          = 0;            %  Internal variable. nntest sets this to one.
+%     nn.output                           = 'sigm';
+%    nn.learningRate = 2;
 
     % this neural network implementation requires number of samples to be a
     % multiple of batchsize, so we remove some for this to be true.
@@ -67,5 +93,14 @@ function [ classVoteTr, classVoteTe ] = trainModelNN( Tr, Te, labels, param )
         classVoteTr(classVotePredTr == i) = labels(i);
         classVoteTe(classVotePredTe == i) = labels(i);
     end
+    
+    
+    global counterI
+    if exist('counterI')
+        counterI = counterI+1;
+    else
+        counterI = 0;
+    end
+    saveas(gcf,['prediction', num2str(counterI),'.png']);
 end
 
--- visualizeLearningCurve.m
+++ visualizeLearningCurve.m
@@ -1,17 +1,36 @@
 clc;
+figure;
+hold on;
 
 load('recodingGlobalTe.mat');
+load('recodingGlobalTr.mat');
 
-allK = 2:10;
-datasetSize = 100 - 100./allK;
+% allK = 2:10;
+% datasetSize = 100 - 100./allK;
+% x = datasetSize*60;
 
-x = datasetSize*60;
-y = mean(globalEvaluationTe);
-e = std(globalEvaluationTe);
+x = 0.02:0.005:0.065;
+
+%y = mean(globalEvaluationTe);
+%e = std(globalEvaluationTe
+
+y = mean(g1te);
+e = std(g1te);
 
 errorbar(x,y,e);
+
+%y = mean(globalEvaluationTr);
+%e = std(globalEvaluationTr);
+y = mean(g1tr);
+e = std(g1tr);
+errorbar(x,y,e);
+
 title('Learning curve');
 ylabel('BER');
-xlabel('Number of training sample');
+xlabel('L2 penality');
+
+legend('Testing','Training');
 %curtick = get(gca, 'XTick');
 %set(gca, 'XTickLabel', cellstr(num2str(curtick(:))));
+
+
